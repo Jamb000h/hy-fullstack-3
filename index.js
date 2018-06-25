e456814fc56dc4f14b766c6982825aa5d2f4fdb1
Jonne Kanerva <jonne.kanerva@siili.com>
2018-06-25 14:49:56 +0300

    Tehtävä 3.7

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 const app = express()
+
+app.use(morgan('tiny'))
 app.use(bodyParser.json())
 
 const json = {
@@ -101,3 +104,5 @@ app.get('/info', (req, res) => {
 const PORT = 3001
 
 app.listen(PORT)
+
+console.log('Server running on port', PORT)
